df7c89bc1b99c47395fe79f4d4f69b0dcc502a86
Nishant Sardana <nishantsardana000@gmail.com>
2025-02-17 20:58:56 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,29 +1,39 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    char c;
-    scanf("%d %d %c",&a,&b,&c);
-    if(c=='+'){
-        printf("%d",a+b);
-    }
-    else if(c=='-'){
-        printf("%d",a-b);
-    }
-    else if(c=='*'){
-        printf("%d",a*b);
-    }
-    else if(c== '/'){
-        if(b==0){
+#include <stdio.h>
+
+int main() {
+    double num1, num2;   // Declare two double variables for operands
+    char operator;       // Declare a variable for the operator
+
+    // Ask the user to input the two numbers and the operator
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // Space before %c to capture any leading whitespaces
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    // Perform the operation based on the operator input
+    if (operator == '+') {
+        printf("Result: %.2lf\n", num1 + num2);
+    } 
+    else if (operator == '-') {
+        printf("Result: %.2lf\n", num1 - num2);
+    } 
+    else if (operator == '*') {
+        printf("Result: %.2lf\n", num1 * num2);
+    } 
+    else if (operator == '/') {
+        if (num2 != 0) {
+            printf("Result: %.2lf\n", num1 / num2);
+        } else {
             printf("error");
         }
-        else{
-            printf("%d",a/b);
-        }
-    }
-    else{
+    } 
+    else {
         printf("error");
     }
-
 
     return 0;
 }
