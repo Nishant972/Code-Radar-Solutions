594d7baa3ee2deec353448da7c355d9a4278892b
Nishant Sardana <nishantsardana000@gmail.com>
2025-03-21 19:58:30 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,18 +3,6 @@
 
 void fibonacci(int n);
 
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    if(n <= 0) {
-        printf("Invalid input\n");
-        return 0;
-    } else {
-        fibonacci(n);
-    }
-    return 0;
-}
 
 void fibonacci(int n) {
     int a = 0, b = 1, c;
@@ -28,7 +16,7 @@ void fibonacci(int n) {
 
     for(int i = 3; i <= n; i++) {
         c = a + b;
-        printf("%d ", c);
+        printf("%d ", c);   
         a = b;
         b = c;
     }
