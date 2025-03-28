a7ed69472353482d3ef625999871ad03ef78e011
Nishant Sardana <nishantsardana000@gmail.com>
2025-03-28 11:08:29 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void shifttoright(int arr[],int n){
     int last=arr[n-1];
-    for(int i=0;i<n;i++){
+    for(int i=n-1;i>0;i--){
         arr[i]=arr[i-1];
     }
     arr[0]=last;
